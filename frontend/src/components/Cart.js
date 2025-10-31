0e46386b29e3ddf74df5a1c4ba817c9f687d0512
premothaniruth <premothaniruthtg@gmail.com>
2025-10-31 23:11:20 +0530

    fix 25

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState, useCallback } from "react";
 import { fetchSectionsMeta } from "../api";
 
 /**
